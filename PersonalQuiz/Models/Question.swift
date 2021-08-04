433246ce3e5e4dbcf61a5547daf35f648ea11ffe
byMaxKucher <63538024+MaxDevIos@users.noreply.github.com>
2021-08-04 11:41:00 +0300

    Models ready

---
+++ PersonalQuiz/Models/Question.swift
@@ -0,0 +1,55 @@
+//
+//  Question.swift
+//  PersonalQuiz
+//
+//  Created by Maxim Kucherov on 03.08.2021.
+//
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
+
+struct Question {
+    let text: String
+    let type: ResponseType
+    let answers: [Answer]
+}
+
+extension Question {
+    static func getQuestion() -> [Question] {
+        [
+            Question(
+                text: "Какую пищу вы предпочитаете?",
+                type: .single,
+                answers: [
+                    Answer(text: "Стейк", type: .dog),
+                    Answer(text: "Рыба", type: .cat),
+                    Answer(text: "Морковь", type: .rubbit),
+                    Answer(text: "Кукуруза", type: .turtle),
+                ]
+            ),
+            Question(
+                text: "Что вам нравиться больше?",
+                type: .multiple,
+                answers: [
+                    Answer(text: "Плавать", type: .dog),
+                    Answer(text: "Спать", type: .cat),
+                    Answer(text: "Обниматься", type: .rubbit),
+                    Answer(text: "Есть", type: .turtle),
+                ]
+            ),
+            Question(
+                text: "Любите ли вы поездки на машине?",
+                type: .ranged,
+                answers: [
+                    Answer(text: "Ненавижу", type: .cat),
+                    Answer(text: "Нервничаю", type: .rubbit),
+                    Answer(text: "Не замечаю", type: .turtle),
+                    Answer(text: "Обожаю", type: .dog),
+                ]
+            )
+        ]
+    }
+}
